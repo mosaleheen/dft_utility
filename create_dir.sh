8e5fc99f5a559617f0a9e3a12313687dd4e8d88f
Mohammad Saleheen <saleheen@email.sc.edu>
2021-05-18 13:10:36 -0400

    modified create_dir

--- create_dir.sh
+++ create_dir.sh
@@ -3,16 +3,13 @@
 # Purpose: For any files with .cell extension, create same named directory
 #          and generate a POSCAR
 
-script_dir="/home/saleheen/guaiacol_scripts"
-#copy_from_dir="/work/saleheen/02_guaiacol_hdo/DIMERSource"
-copy_from_dir="/work/saleheen/02_guaiacol_hdo/VOPTSource"
+copy_from_dir="/work/saleheen/source_inputs/vasp_opt"
 
 for i in *.cell; 
 	do nam=`echo $i | sed 's/.cell//g'`; 
 	mkdir $nam; 
 	mv $nam.cell $nam/;
 	cd $nam;
-	$script_dir/direct_poscar.sh
         cp $copy_from_dir/* .
 	cd ../
 done
